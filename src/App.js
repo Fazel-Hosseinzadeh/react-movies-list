dcc44cda090a32f0e395afb68190b6c2ae5cd5d7
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2025-04-07 18:17:09 +0200

    Add MovieCard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import MovieCard from './components/MovieCard/MovieCard'
 
-function App(){
+function App() {
+  const movie = {
+    id: 1,
+    title: "Transporter 1",
+    image_URL: "https://www.google.com",
+    year: "1999",
+  }
+
   return (
     <>
-      <div>Hello World!</div>
+      < MovieCard movie = {movie} />
     </>
   );
 }
---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,21 @@
+
+function MovieCard(props){
+    const {id, title, image_URL, year} = props.movie;
+    return (
+        <>
+            <div className="card-container">
+                <div className="poster">
+                    <img src={image_URL} alt={title} />
+                    <button className="movie-like">♥</button>
+                </div>
+                <div className="movie-info">
+                    <div className="movie-title">{title}</div>
+                    <div className="movie-year">{year}</div>
+                </div>
+            </div>
+        </>
+
+    );
+}
+
+export default MovieCard;
