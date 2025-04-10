bfe8e47a7fba93671f1f5de8558b57cbe8a67522
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2025-04-10 09:11:47 +0200

    Add  premitive function for like button in MovieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,13 +2,16 @@ import './MovieCard.css'
 
 function MovieCard(props){
     const {id, title,  year,img_URL} = props.movie;
+    function onFavoritClicked(movie){
+        alert(`clicked on ${title}`)
+    }
     return (
         <>
             <div className="container">
                 <div className="card-container">
                     <div className="poster">
                         <img src={img_URL} alt={title} />
-                        <button className="movie-like">♥</button>
+                        <button className="movie-like" onClick = {()=>onFavoritClicked(title)}>♥</button>
                     </div>
                     <div className="movie-info">
                         <div className="movie-title">{title}</div>
