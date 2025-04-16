748b12b1beda702bd82368ab22cd4e85f00b8b94
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2025-04-16 18:59:50 +0200

    Add FavoriteProvider for wrapping around  Home and Favorites pages to share states and functions

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import NavBar from "./components/Navbar";
 import Favorites from "./pages/Favorites";
 import Home from './pages/Home'
 import {Routes , Route} from 'react-router-dom'
+import FavoriteProvider from "./contexts/FavoritesContext";
 
 function App() {
   return (
-    <>
+    <FavoriteProvider>
     <div className="nav-bar">
       <NavBar/>
     </div>
@@ -15,7 +16,7 @@ function App() {
          <Route path="/" Component={Home} />
          <Route path="/favorites" Component={Favorites} />
       </Routes>
-    </>
+    </FavoriteProvider>
   );
 }
 
