21bb55465111f15621626c1b68eccc29ff05781f
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2025-04-22 19:33:30 +0200

    Home, deactivatiing API and using static data

--- src/service/movies.js
+++ src/service/movies.js
@@ -1,7 +1,9 @@
+
+const movies = await process.env.REACT_APP_MOVIES
 async function Movies() {
 
 try{
-  const movies = await fetch('/data.json')
+  // const movies = await fetch('/data.json')
   return await movies.json();
 
 }catch(err){
