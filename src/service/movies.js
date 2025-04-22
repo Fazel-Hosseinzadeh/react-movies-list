28b2b3506229b1a9db57dd7a3f8d1c07ef2c3979
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2025-04-22 19:48:00 +0200

    Deploy test2, using env file in order to use static file instead of API

--- src/service/movies.js
+++ src/service/movies.js
@@ -1,9 +1,7 @@
 
 async function Movies() {
-  const movies = await process.env.REACT_APP_MOVIES
-
 try{
-  // const movies = await fetch('/data.json')
+  const movies = await fetch('/data.json')
   return await movies.json();
 
 }catch(err){
