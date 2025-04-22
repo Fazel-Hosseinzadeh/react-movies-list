86a544a3839a74f5fda0a117c48ffd3d715a9d60
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2025-04-22 11:58:59 +0200

    Make a folder for Navbar component and modify the Navar address in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/Navbar/Navbar.jsx";
 import Favorites from "./pages/Favorites";
 import Home from './pages/Home'
 import {Routes , Route} from 'react-router-dom'
